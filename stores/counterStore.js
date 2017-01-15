7cfbfa832d4441bcd352c86d4f62eed537af97d0
Matthew <pickledyamsman@gmail.com>
2017-01-15 17:24:15 -0500

    Done.

--- stores/counterStore.js
+++ stores/counterStore.js
@@ -1,6 +1,19 @@
-class CounterStore {
-  // Your implementation here.
-  // Hint: extend the Store class!
+const Store = require('./Store')
+
+class CounterStore extends Store {
+  
+  constructor () {
+    super()
+    this.state = 0
+  }
+
+  increment () {
+    this.state++
+  }
+
+  decrement () {
+    this.state--
+  }
 }
 
 module.exports = new CounterStore();
