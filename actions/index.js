244a1f36d2ad1d6d013b04276e2ffb96f9c6b658
zacscodingclub <zbaston@gmail.com>
2017-02-22 11:25:28 -0500

    Done.

--- actions/index.js
+++ actions/index.js
@@ -1,4 +1,16 @@
+'use strict';
+
+const counterStore = require('../stores/counterStore');
+
+function increment() {
+  counterStore.increment();
+}
+
+function decrement(e) {
+  counterStore.decrement();
+}
+
 module.exports = {
-  // Your implementation here.
-  // Hint: We need two actions!
+  increment,
+  decrement
 };
